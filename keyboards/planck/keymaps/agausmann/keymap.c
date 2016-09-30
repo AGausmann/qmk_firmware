feafe4acf2283f0b2bb4c198d53376a28a2cce3f
Adam Gausmann <adam@nonemu.ninja>
2016-09-30 15:50:47 -0500

    Swap layers so typing is default

--- keyboards/planck/keymaps/agausmann/keymap.c
+++ keyboards/planck/keymaps/agausmann/keymap.c
@@ -12,17 +12,17 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define DEFAULT_MAP 0
-#define DEFAULT_NUMPAD_MAP 1
-#define TYPING_MAP 10
-#define LOWER_MAP 11
-#define RAISE_MAP 12
-#define TYPING_NUMPAD_MAP 13
+#define LOWER_MAP 1
+#define RAISE_MAP 2
+#define DEFAULT_NUMPAD_MAP 3
+#define GAMING_MAP 10
+#define GAMING_NUMPAD_MAP 11
 #define ADJUST_MAP 20
 
 enum planck_keycodes {
-  CK_TYPE = TG(TYPING_MAP),
+  CK_GAME = TG(GAMING_MAP),
   CK_DNUM = MO(DEFAULT_NUMPAD_MAP),
-  CK_TNUM = MO(TYPING_NUMPAD_MAP),
+  CK_GNUM = MO(GAMING_NUMPAD_MAP),
   CK_LOW = SAFE_RANGE,
   CK_RAS,
 };
@@ -33,24 +33,10 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [DEFAULT_MAP] = {
-  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP  },
-  {KC_LCTL, _______, KC_LALT, _______, CK_DNUM, KC_SPC,  KC_B, CK_GRV,  CK_TYPE, KC_LEFT, KC_RGHT, KC_DOWN}
-},
-
-[DEFAULT_NUMPAD_MAP] = {
-  {_______, _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______},
-  {_______, KC_0,    KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
-
-[TYPING_MAP] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_LCTL, KC_LGUI, KC_LALT, CK_TNUM, CK_LOW,  KC_SPC,  KC_SPC,  CK_RAS,  CK_TYPE, KC_RALT, KC_RGUI, KC_RCTL}
+  {KC_LCTL, KC_LGUI, KC_LALT, CK_DNUM, CK_LOW,  KC_SPC,  KC_SPC,  CK_RAS,  CK_GAME, KC_RALT, KC_RGUI, KC_RCTL}
 },
 
 [LOWER_MAP] = {
@@ -67,10 +53,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
-[TYPING_NUMPAD_MAP] = {
+[DEFAULT_NUMPAD_MAP] = {
   {_______, _______, _______, _______, _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   _______, _______},
   {_______, _______, KC_PAST, KC_PPLS, _______, _______, KC_PDOT, KC_P4,   KC_P5,   KC_P6,   _______, _______},
   {_______, _______, KC_PSLS, KC_PMNS, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+},
+
+[GAMING_MAP] = {
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP  },
+  {KC_LCTL, _______, KC_LALT, _______, CK_GNUM, KC_SPC,  KC_B,    KC_GRV,  CK_GAME, KC_LEFT, KC_RGHT, KC_DOWN}
+},
+[GAMING_NUMPAD_MAP] = {
+  {_______, _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______},
+  {_______, KC_0,    KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
