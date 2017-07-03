42a4a6b3c6ffc3e917ea27cad180d8a65011f883
Adam Gausmann <adam@nonemu.ninja>
2017-07-03 02:02:28 -0500

    Moved numpad over one column (no clue why)

--- keyboards/planck/keymaps/agausmann/keymap.c
+++ keyboards/planck/keymaps/agausmann/keymap.c
@@ -42,9 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_LCTL, KC_LGUI, KC_LALT, CK_DNUM, CK_LOW,  KC_SPC,  KC_SPC,  CK_RAS,  XXXXXXX, KC_RALT, KC_RGUI, KC_RCTL}
+  {KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, CK_RAS,  KC_SPC,  KC_SPC,  CK_LOW,  CK_DNUM, KC_RALT, KC_RGUI, KC_RCTL}
 },
-
 [LOWER_MAP] = {
   {KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______},
   {_______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______},
@@ -60,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 [DEFAULT_NUMPAD_MAP] = {
-  {_______, _______, _______, _______, _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   _______, _______},
-  {_______, _______, KC_PAST, KC_PPLS, _______, _______, KC_PDOT, KC_P4,   KC_P5,   KC_P6,   _______, _______},
-  {_______, _______, KC_PSLS, KC_PMNS, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   _______},
+  {_______, _______, KC_PAST, KC_PPLS, _______, _______, _______, KC_PDOT, KC_P4,   KC_P5,   KC_P6,   _______},
+  {_______, _______, KC_PSLS, KC_PMNS, _______, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
@@ -82,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [ADJUST_MAP] = {
   {RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, BL_DEC,  BL_INC,  _______, _______, CK_DFLT, CK_GAME, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, MI_ON,   MI_OFF,  _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
 };
